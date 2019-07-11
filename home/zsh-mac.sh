98486e6488be972220909035636081e6952fe8cb
idwangmo <idwangmo@gmail.com>
2019-07-11 11:25:27 +0800

    fix(zsh): modify all zsh config

--- home/zsh-mac.sh
+++ home/zsh-mac.sh
@@ -2,32 +2,38 @@
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH=/Users/idwangmo/.oh-my-zsh
+export ZSH="/Users/idwangmo/.oh-my-zsh"
 
-# Set name of the theme to load. Optionally, if you set this to "random"
-# it'll load a random theme each time that oh-my-zsh is loaded.
+# Set name of the theme to load --- if set to "random", it will
+# load a random theme each time oh-my-zsh is loaded, in which case,
+# to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
 ZSH_THEME="ys"
 
-# Set list of themes to load
-# Setting this variable when ZSH_THEME=random
-# cause zsh load theme from this variable instead of
-# looking in ~/.oh-my-zsh/themes/
-# An empty array have no effect
+# Set list of themes to pick from when loading at random
+# Setting this variable when ZSH_THEME=random will cause zsh to load
+# a theme from this variable instead of looking in ~/.oh-my-zsh/themes/
+# If set to an empty array, this variable will have no effect.
 # ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
 
 # Uncomment the following line to use case-sensitive completion.
 # CASE_SENSITIVE="true"
 
-# Uncomment the following line to use hyphen-insensitive completion. Case
-# sensitive completion must be off. _ and - will be interchangeable.
+# Uncomment the following line to use hyphen-insensitive completion.
+# Case-sensitive completion must be off. _ and - will be interchangeable.
 # HYPHEN_INSENSITIVE="true"
 
 # Uncomment the following line to disable bi-weekly auto-update checks.
 # DISABLE_AUTO_UPDATE="true"
 
+# Uncomment the following line to automatically update without prompting.
+# DISABLE_UPDATE_PROMPT="true"
+
 # Uncomment the following line to change how often to auto-update (in days).
-export UPDATE_ZSH_DAYS=3
+# export UPDATE_ZSH_DAYS=13
+
+# Uncomment the following line if pasting URLs and other text is messed up.
+# DISABLE_MAGIC_FUNCTIONS=true
 
 # Uncomment the following line to disable colors in ls.
 # DISABLE_LS_COLORS="true"
@@ -48,21 +54,22 @@ export UPDATE_ZSH_DAYS=3
 
 # Uncomment the following line if you want to change the command execution time
 # stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-HIST_STAMPS="yyyy-mm-dd"
+# You can set one of the optional three formats:
+# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
+# or set a custom format using the strftime function format specifications,
+# see 'man strftime' for details.
+# HIST_STAMPS="mm/dd/yyyy"
 
 # Would you like to use another custom folder than $ZSH/custom?
 # ZSH_CUSTOM=/path/to/new-custom-folder
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
+# Which plugins would you like to load?
+# Standard plugins can be found in ~/.oh-my-zsh/plugins/*
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(
-  git
-  docker
-  osx
-)
+plugins=(git
+	 osx)
 
 source $ZSH/oh-my-zsh.sh
 
@@ -72,9 +79,6 @@ source $ZSH/oh-my-zsh.sh
 
 # You may need to manually set your language environment
 export LANG=en_US.UTF-8
-
-# pipenv lang config
-export LC_ALL=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
 # if [[ -n $SSH_CONNECTION ]]; then
@@ -89,57 +93,16 @@ export ARCHFLAGS="-arch x86_64"
 # Set personal aliases, overriding those provided by oh-my-zsh libs,
 # plugins, and themes. Aliases can be placed here, though oh-my-zsh
 # users are encouraged to define aliases within the ZSH_CUSTOM folder.
-alias zshconfig="emacs ~/.zshrc"
-alias ohmyzsh="emacs ~/.oh-my-zsh"
-alias source-zsh="source ~/.zshrc"
+# For a full list of active aliases, run `alias`.
+#
+alias zshconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias sourcezsh="source ~/.zshrc"
+alias anniex="annie -x http://127.0.0.1:7890"
 
-# keymap
-# brew
-alias brew-update='brew update && brew upgrade && \
-    brew cask upgrade && brew cleanup'
-
-# docker
-alias docker-kill='docker kill $(docker ps -a -q)'
-alias docker-clean-container='docker rm $(docker ps -a -q)'
-alias docker-clean-image='docker rmi $(docker images -q -f dangling=true)'
-alias docker-clean-all='dockercleanc && dockercleani && \
-    docker rmi -f $(docker images -q)'
-
-# other keymap
-alias nvim-config='nvim $HOME/.vim/init.vim'
-
-# miniconda config
-export PATH=/usr/local/miniconda3/bin:$PATH
-
-# fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-# the fuck
-eval $(thefuck --alias)
-eval $(thefuck --alias FUCK)
-
-# default editor
-export EDITOR="emacs"
-
-# doom config
-export PATH=$PATH:$HOME/.emacs.d/bin
-
-# brew config
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.tuna.tsinghua.edu.cn/homebrew-bottles
+# homebrew config
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 export PATH="/usr/local/sbin:$PATH"
 
-# MYSQL config
-export PATH="/usr/local/opt/mysql-client/bin:$PATH"
-
-# sqlite3 config
-export PATH="/usr/local/opt/sqlite/bin:$PATH"
-
-# go config
-export PATH=$PATH:$HOME/go/bin
-
-# gpg key
-export GPG_TTY=$(tty)
-
-# llvm config
-export PATH="/usr/local/opt/llvm/bin:$PATH"
-
+# doom emacs config
+export PATH="$PATH:$HOME/.emacs.d/bin"
