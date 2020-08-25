0c19ca69850acfd58d4ff4505c5d6b099386b41e
idwangmo <idwangmo@protonmail.com>
2020-08-25 15:31:55 +0800

    feat: 🎸 zsh
    add ubuntu zsh plugin

--- home/zsh/zshrc-ubuntu.sh
+++ home/zsh/zshrc-ubuntu.sh
@@ -68,7 +68,7 @@ ZSH_THEME="ys"
 # Custom plugins may be added to $ZSH_CUSTOM/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git docker fzf-tab)
+plugins=(git docker fzf-tab ubuntu)
 
 source $ZSH/oh-my-zsh.sh
 
