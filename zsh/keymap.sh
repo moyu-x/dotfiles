d1df9d00ebcf1f6a5e53753805dfa7caa6c104d3
idwangmo <idwangmo@protonmail.com>
2021-04-21 10:43:58 +0800

    fix: 🐛 zsh
    modify and keymap

--- zsh/keymap.sh
+++ zsh/keymap.sh
@@ -6,5 +6,6 @@ alias sourcezsh="source ~/.zshrc"
 alias nvimconfig='nvim ~/.config/nvim/init.vim'
 alias dotfiles='nvim ~/.dotfiles'
 alias ls="exa"
+alias vim="nvim"
 alias anniex="annie -x http://127.0.0.1:7890"
 alias merge-video="rename ' ' '' * | find * | xargs -i echo file {} > temp.txt | ffmpeg -safe 0 -f concat -i temp.txt -c copy output.mp4; rm temp.txt"
