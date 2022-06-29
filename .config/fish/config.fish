fd74509a6ffeda8f75d36880ba285b5cbb9b1943
moyu <idwangmo@protonmail.com>
2022-06-29 20:18:50 +0800

    feat(fish): fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,11 @@ end
 
 set -x PATH $HOME/.local/bin $HOME/.cargo/bin $HOME/go/bin $PATH
 set -x GOPROXY https://proxy.golang.com.cn,direct
+set -x HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
+set -x HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
+set -x HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
+set -x LC_ALL en_US.UTF-8
+set -x LANG zh_CN.UTF-8 
 
 alias ls exa
 alias vim nvim
