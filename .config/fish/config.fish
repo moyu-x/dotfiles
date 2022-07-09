a195bf76c92950590872f793f54d02e0ac53e84a
idwangmo <idwangmo@protonmail.com>
2022-07-10 00:56:55 +0800

    feat(vim): use spacevim on neovim

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ set -x GOPROXY https://proxy.golang.com.cn,direct
 set -x HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
 set -x HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
 set -x HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
+set -x HOMEBREW_NO_AUTO_UPDATE 1
 set -x LC_ALL en_US.UTF-8
 set -x LANG zh_CN.UTF-8 
 
